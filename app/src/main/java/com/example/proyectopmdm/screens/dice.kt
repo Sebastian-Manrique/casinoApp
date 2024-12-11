7d024f913578599a0b53a263c697e8bb73f7972d
Sebastian-Manrique <smanriquemontiel@gmail.com>
2024-12-11 20:39:57 +0100

    Adding more functions and things to the dice page

--- app/src/main/java/com/example/proyectopmdm/screens/dice.kt
+++ app/src/main/java/com/example/proyectopmdm/screens/dice.kt
@@ -1,20 +1,25 @@
 package com.example.proyectopmdm.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -28,10 +33,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun Dice(navController: NavHostController) {
@@ -41,12 +52,23 @@ fun Dice(navController: NavHostController) {
 
     Column(
         modifier = Modifier
-            .fillMaxWidth().fillMaxHeight(),
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(Color(0xFF007c57)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text("Choose your lucky number \uD83C\uDF40", fontSize = 20.sp)
 
-        Text("amount to bet")
+        Spacer(Modifier.height(10.dp))
+
+        GamblingNumbersGrid()
+
+        Spacer(Modifier.height(10.dp))
+
+        Text("amount to bet", fontSize = 20.sp)
+
+        Spacer(Modifier.height(10.dp))
 
         var text2 by remember { mutableStateOf("") } // Holds the input text
         TextField(
@@ -55,33 +77,53 @@ fun Dice(navController: NavHostController) {
             label = { Text("Enter the amount") },
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
-        Text("Currently money $money")
-        Button(onClick = {
-            resultNumber = randomGenerator(list)
-            if (resultNumber == 3) money += 5 else money -= 5
-        }) {
+        Spacer(Modifier.height(5.dp))
+
+        Text("Currently money $money€", fontSize = 17.sp)
+
+        Spacer(Modifier.height(5.dp))
+
+        Button(
+            onClick = {
+                resultNumber = randomGenerator(list)
+
+            },
+            //No se puede simplificar el color.
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF379ca0))
+        ) {
             Text("Roll the dices! 🎲")
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "The result is $resultNumber")
+        Text(text = "The result is $resultNumber", fontSize = 20.sp)
 
         Spacer(modifier = Modifier.height(16.dp))
+
+        val gradientBrush = Brush.horizontalGradient(
+            colors = listOf(Color.Black, Color(0xFF006b4b)),
+            startX = 0.0f,
+            endX = 1000.0f,
+            tileMode = TileMode.Repeated
+        )
 
         Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
-                .background(Color(0xff181a17))
+                .border(
+                    width = 2.dp,
+                    brush = gradientBrush,
+                    shape = CircleShape
+                )
                 .clickable { isExpanded = !isExpanded } // Alterna la expansión al hacer clic
-                .padding(16.dp)
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
         ) {
-            Column {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
                     text = if (isExpanded) "Hide Results" else "Show Results",
-                    style = MaterialTheme.typography.labelMedium,
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                    style = MaterialTheme.typography.labelMedium
                 )
                 if (isExpanded) {
                     LazyColumn(
@@ -92,7 +134,8 @@ fun Dice(navController: NavHostController) {
                         items(list) { item ->
                             Text(
                                 text = "Rolled: $item",
-                                modifier = Modifier.padding(8.dp)
+                                modifier = Modifier.padding(8.dp),
+                                textAlign = TextAlign.Center
                             )
                         }
                     }
@@ -102,8 +145,60 @@ fun Dice(navController: NavHostController) {
     }
 }
 
+// Vista previa que solo muestra la cuadrícula
+@Preview(showBackground = true)
+@Composable
+fun Ver() {
+    val navController = rememberNavController()
+    Dice(navController)
+}
+
+@Composable
+fun GamblingNumbersGrid() {
+    // Crear un estado mutable para almacenar los colores de fondo de cada celda
+    val colors = remember {
+        mutableStateListOf<Color>().apply {
+            repeat(12) { add(if (it % 2 == 0) Color.Red else Color.Black) }
+        }
+    }
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(6), // 6 columnas
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(width = 2.dp, color = Color.Black)
+    ) {
+        items(12) { index -> // 12 elementos (2 columnas * 6 filas)
+            Box(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .size(100.dp) // Tamaño de cada celda
+                    .background(colors[index]) // Usar el color específico para cada celda
+                    .clickable {
+                        colors[index] =
+                            if (colors[index] == Color.Red || colors[index] == Color.Black) {
+                                Color(0xFF262e2c) // Negro apagado
+                            } else {
+                                if (index % 2 == 0) Color.Red else Color.Black // Restaurar al color original
+                            }
+                        println("El número seleccionado es ${index + 1}")
+                    },
+            ) {
+                Text(
+                    "${index + 1}",
+                    color = Color.White,
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(alignment = Alignment.Center)
+                )
+            }
+        }
+    }
+}
+
+
 fun randomGenerator(list: SnapshotStateList<Int>): Int {
-    val number = (1..6).random()
+    val number = (1..12).random()
     list.add(number)
     return number
 }
